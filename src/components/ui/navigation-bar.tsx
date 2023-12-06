9e6a00fd94e2148560f47b7002d69e2cd8db199d
Nikhil <tymnec@gmail.com>
2023-12-06 20:14:23 +0530

    added categories menu and resolves #1

--- src/components/ui/navigation-bar.tsx
+++ src/components/ui/navigation-bar.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Button } from "./button";
 import { Input } from "./input";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 // Structure of Navigation bar
 // Navigation:
@@ -15,15 +23,27 @@ function NavigationBar() {
   return (
     <div className="flex w-full p-3">
       {/* Logo */}
-      <div className="p-1 font-serif border border-red-600">
+      <div className="p-1 font-serif border border-red-600 mx-2">
         <p>Logo</p>
       </div>
       {/* List of navigation buttons */}
-      <div className="flex w-full justify-start">
-        <Button variant="link">Home</Button>
-        <Button variant="link">About</Button>
-        <Button variant="link">Contact</Button>
-        <Button variant="link">Categories</Button>
+      <div className="flex w-full justify-start gap-2 p-1">
+        <Button variant={"default"}>Home</Button>
+        <Button variant={"ghost"}>About</Button>
+        <Button variant={"ghost"}>Contact</Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant={"ghost"}>Categories</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Categories</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>First Category</DropdownMenuItem>
+            <DropdownMenuItem>Second Category</DropdownMenuItem>
+            <DropdownMenuItem>Third Category</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       <div className="w-96 flex justify-end gap-8">
